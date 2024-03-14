fbb0503dee0d1c7233f1092053b9a6dfc0eb2282
soheil <140661028+soheilsolim@users.noreply.github.com>
2024-03-14 10:56:21 -0700

    Add files via upload

---
+++ convert_MILE_to_METER.m
@@ -0,0 +1,7 @@
+%soheil mansouri (electrical engineering)
+%Script file to convert miles to meter
+%
+MILE=input('number in MILE:');  %give MILE
+METER=(MILE*1609.350);
+disp(['number in METER=',num2str(METER)])  %get METER
+%stop
